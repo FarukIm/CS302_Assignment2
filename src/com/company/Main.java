5a72b46a99a9686b2c2d31fd886365d59f9d8092
FarukIm <f.imamovic27@gmail.com>
2021-11-06 20:34:59 +0100

    adding all classes

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public boolean isPalindrome(Node list){
+        return true;
+    }
+    public static void main(String[] args) {
+	    Node list = new Node(1);
+        list.next = null;
+
+
+    }
+}
---
+++ src/com/company/Node.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+public class Node {
+
+    public int data;
+    public Node next;
+
+    Node(int num){
+        data = num;
+        next = null;
+    }
+
+}
