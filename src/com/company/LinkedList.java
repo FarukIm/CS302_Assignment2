4afdf952b4d9cdd972546d7d336a870d8b448adb
FarukIm <f.imamovic27@gmail.com>
2021-11-06 21:44:31 +0100

    added addFront, addRear,traverseFront,traverseBack methods in linkedlist class and added input in main class

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -1,8 +1,9 @@
 package com.company;
 
 public class LinkedList {
-    public Node first;
-    public Node last;
+    public Node first = null;
+    public Node last = null;
+
     public void addFront(int num){
 
         Node temp = new Node(num);
@@ -25,6 +26,22 @@ public class LinkedList {
             temp.previous = last;
             last.next = temp;
             last = temp;
+        }
+    }
+
+    public void traverseFront(){
+        Node current = first;
+        while(current != null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+    public void traverseRear(){
+        Node current = last;
+        while(current != null){
+            System.out.println(current.data);
+            current = current.previous;
         }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,33 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
-    public boolean isPalindrome(Node list){
+    public static boolean isPalindrome(LinkedList list){
+        Node front = list.first;
+        Node back = list.last;
+        while(front != null){
+            if(front.data != back.data){
+                return false;
+            }
+            front = front.next;
+            back = back.previous;
+        }
         return true;
     }
     public static void main(String[] args) {
-	    Node list = new Node(1);
-        list.next = null;
+	    LinkedList list = new LinkedList();
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many nodes do you want to insert");
+        int n = input.nextInt();
+        int i = 0;
+        while(i < n){
+            System.out.print("Add element: ");
+            int k = input.nextInt();
+            list.addRear(k);
+            i++;
+        }
+        System.out.print(isPalindrome(list));
+
 
 
     }
